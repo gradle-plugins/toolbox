5494e6e3ce695cd48542713082c0e5e1765a6375
Daniel Lacasse <daniel@lacasse.io>
2019-07-21 19:49:19 +0400

    Prepare version 0.0.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildScan {
 
 allprojects {
     group = "dev.gradleplugins"
-    version = "0.0.11"
+    version = "0.0.12-SNAPSHOT"
 
     apply<GitHubSourceControlManagerPlugin>()
     configure<GitHubSourceControlManagerExtension> {
