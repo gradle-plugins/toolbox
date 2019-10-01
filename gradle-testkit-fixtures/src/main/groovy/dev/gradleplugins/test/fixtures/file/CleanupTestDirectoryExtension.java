2cb51792d3d6d8d7cff140029d913d5a259f0533
Daniel Lacasse <daniel@lacasse.io>
2019-10-01 17:28:38 +0200

    Migrate Groovy code to Java

---
+++ gradle-testkit-fixtures/src/main/groovy/dev/gradleplugins/test/fixtures/file/CleanupTestDirectoryExtension.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.gradleplugins.test.fixtures.file;
+
+import org.spockframework.runtime.AbstractRunListener;
+import org.spockframework.runtime.GroovyRuntimeUtil;
+import org.spockframework.runtime.IRunListener;
+import org.spockframework.runtime.extension.AbstractAnnotationDrivenExtension;
+import org.spockframework.runtime.extension.IMethodInterceptor;
+import org.spockframework.runtime.extension.IMethodInvocation;
+import org.spockframework.runtime.model.ErrorInfo;
+import org.spockframework.runtime.model.FeatureInfo;
+import org.spockframework.runtime.model.SpecInfo;
+
+public class CleanupTestDirectoryExtension extends AbstractAnnotationDrivenExtension<CleanupTestDirectory> {
+    @Override
+    public void visitSpecAnnotation(CleanupTestDirectory annotation, SpecInfo spec) {
+        spec.getFeatures().forEach((FeatureInfo feature) -> {
+            feature.addIterationInterceptor(new FailureCleanupInterceptor(annotation.fieldName()));
+        });
+    }
+
+    private static class FailureCleanupInterceptor implements IMethodInterceptor {
+        final String fieldName;
+
+        FailureCleanupInterceptor(String fieldName) {
+            this.fieldName = fieldName;
+        }
+
+        @Override
+        public void intercept(IMethodInvocation invocation) throws Throwable {
+            IRunListener noCleanupOnErrorListener = new AbstractRunListener() {
+                @Override
+                public void error(ErrorInfo error) {
+                    TestDirectoryProvider provider = (TestDirectoryProvider) GroovyRuntimeUtil.getProperty(invocation.getInstance(), fieldName);
+                    provider.suppressCleanup();
+                }
+            };
+            SpecInfo spec = invocation.getSpec();
+            while (spec != null) {
+                spec.addListener(noCleanupOnErrorListener);
+                spec = spec.getSubSpec();
+            }
+            invocation.proceed();
+        }
+    }
+}
