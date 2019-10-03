731d22a9747ad3691f5e2a5edf3c8b050fb8af99
Daniel Lacasse <daniel@lacasse.io>
2019-10-03 12:02:39 +0200

    Move gradle-plugin-development project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 rootProject.name = "toolbox"
 
-include("gradle-plugin-development")
 include("gradle-testkit-fixtures")
 
 // Gradle APIs
@@ -12,6 +11,10 @@ include("license-gradle-plugin")
 project(":license-gradle-plugin").projectDir = file("subprojects/license-gradle-plugin")
 include("scm-github-gradle-plugin")
 project(":scm-github-gradle-plugin").projectDir = file("subprojects/scm-github-gradle-plugin")
+
+// Plugin developement
+include("gradle-plugin-development")
+project(":gradle-plugin-development").projectDir = file("subprojects/gradle-plugin-development")
 include("gradle-plugin-development-annotation")
 project(":gradle-plugin-development-annotation").projectDir = file("subprojects/gradle-plugin-development-annotation")
 include("gradle-plugin-development-processor")
