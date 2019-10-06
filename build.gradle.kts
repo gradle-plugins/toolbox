6eab9419b754c05f0bae81ec378b476186a1df00
Daniel Lacasse <daniel@lacasse.io>
2019-10-06 19:32:09 +0200

    Release version 0.0.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildScan {
 
 allprojects {
     group = "dev.gradleplugins"
-    version = "0.0.13-SNAPSHOT"
+    version = "0.0.13"
 
     apply<GitHubSourceControlManagerPlugin>()
     configure<GitHubSourceControlManagerExtension> {
