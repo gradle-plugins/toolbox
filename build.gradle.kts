31947e139367131525dc000b2d633bf30bb98d0f
Daniel Lacasse <daniel@lacasse.io>
2019-10-07 01:20:58 +0200

    Prepare version 0.0.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildScan {
 
 allprojects {
     group = "dev.gradleplugins"
-    version = "0.0.21"
+    version = "0.0.22-SNAPSHOT"
 
     apply<GitHubSourceControlManagerPlugin>()
     configure<GitHubSourceControlManagerExtension> {
