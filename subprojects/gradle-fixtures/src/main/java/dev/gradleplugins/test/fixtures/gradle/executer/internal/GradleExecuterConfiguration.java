980d70132d4d2e68b53d92eda40be2074bc51309
Daniel Lacasse <daniel@lacasse.io>
2020-03-27 11:09:33 -0400

    Ensure the stacktrace is shown by default

--- subprojects/gradle-fixtures/src/main/java/dev/gradleplugins/test/fixtures/gradle/executer/internal/GradleExecuterConfiguration.java
+++ subprojects/gradle-fixtures/src/main/java/dev/gradleplugins/test/fixtures/gradle/executer/internal/GradleExecuterConfiguration.java
@@ -16,7 +16,7 @@ import java.util.function.Consumer;
 public class GradleExecuterConfiguration {
     @With private File workingDirectory = null;
     @With private File userHomeDirectory = null;
-    @With private boolean showStacktrace = false;
+    @With private boolean showStacktrace = true;
     @With private File settingsFile = null;
     @With private File buildScript = null;
     @With @NonNull private List<File> initScripts = new ArrayList<>();
@@ -33,6 +33,6 @@ public class GradleExecuterConfiguration {
     @With @NonNull private List<Consumer<? super GradleExecuter>> afterExecute = new ArrayList<>();
 
     public GradleExecuterConfiguration() {
-        this(null, null, false, null, null, Collections.emptyList(), null, false, Collections.emptyList(), Collections.emptyList(), Collections.emptyMap(), null, false, false, null, Collections.emptyList(), Collections.emptyList());
+        this(null, null, true, null, null, Collections.emptyList(), null, false, Collections.emptyList(), Collections.emptyList(), Collections.emptyMap(), null, false, false, null, Collections.emptyList(), Collections.emptyList());
     }
 }
