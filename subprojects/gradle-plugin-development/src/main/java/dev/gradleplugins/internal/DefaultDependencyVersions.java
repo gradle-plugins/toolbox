cade453ab1ca0a19f315dcc052f8ee6eb02a073d
Daniel Lacasse <daniel@lacasse.io>
2020-04-04 09:25:38 -0400

    Update fixtures to 0.0.35

--- subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/DefaultDependencyVersions.java
+++ subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/DefaultDependencyVersions.java
@@ -3,5 +3,5 @@ package dev.gradleplugins.internal;
 public interface DefaultDependencyVersions {
     String SPOCK_FRAMEWORK_VERSION = "1.2-groovy-2.5";
     String GROOVY_ALL_VERSION = "2.5.10";
-    String GRADLE_FIXTURES_VERSION = "0.0.32";
+    String GRADLE_FIXTURES_VERSION = "0.0.35";
 }
