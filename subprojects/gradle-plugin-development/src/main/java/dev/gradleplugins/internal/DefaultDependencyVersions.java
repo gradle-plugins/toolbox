33fcbdbc409c0cca491318d2c8af375a006f7737
Daniel Lacasse <daniel@lacasse.io>
2020-04-10 15:28:13 -0400

    Upgrade versions

--- subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/DefaultDependencyVersions.java
+++ subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/DefaultDependencyVersions.java
@@ -3,5 +3,5 @@ package dev.gradleplugins.internal;
 public interface DefaultDependencyVersions {
     String SPOCK_FRAMEWORK_VERSION = "1.2-groovy-2.5";
     String GROOVY_ALL_VERSION = "2.5.10";
-    String GRADLE_FIXTURES_VERSION = "0.0.39";
+    String GRADLE_FIXTURES_VERSION = "0.0.40";
 }
