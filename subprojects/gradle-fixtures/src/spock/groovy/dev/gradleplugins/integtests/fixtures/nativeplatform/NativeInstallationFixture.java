e22a3d9815e29df349161b4db3b963e66045ee5d
Daniel Lacasse <daniel@lacasse.io>
2020-04-15 10:14:54 -0400

    Convert NativeInstallationFixture to Java

---
+++ subprojects/gradle-fixtures/src/spock/groovy/dev/gradleplugins/integtests/fixtures/nativeplatform/NativeInstallationFixture.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.gradleplugins.integtests.fixtures.nativeplatform;
+
+import dev.gradleplugins.test.fixtures.file.ExecOutput;
+import dev.gradleplugins.test.fixtures.file.TestFile;
+import org.gradle.internal.os.OperatingSystem;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class NativeInstallationFixture {
+    private final TestFile installDir;
+    private final OperatingSystem os;
+
+	public NativeInstallationFixture(TestFile installDir, OperatingSystem os) {
+        this.installDir = installDir;
+        this.os = os;
+    }
+
+    public ExecOutput exec(Object... args) {
+        assertInstalled();
+        return scriptFile().exec(args);
+    }
+
+    @Nullable
+    private TestFile scriptFile() {
+        if (os.isWindows()) {
+            return TestFile.of(Arrays.stream(installDir.listFiles()).filter(it -> it.isFile() && it.getName().endsWith(".bat")).findFirst().orElse(null));
+        } else {
+            return TestFile.of(Arrays.stream(installDir.listFiles()).filter(it -> it.isFile()).findFirst().orElse(null));
+        }
+    }
+
+    public NativeInstallationFixture assertInstalled() {
+        installDir.assertIsDirectory();
+        final TestFile script = scriptFile();
+        assert script != null;
+
+        TestFile libDir = installDir.file("lib");
+        libDir.assertIsDirectory();
+        libDir.file(os.getExecutableName(script.getName())).assertIsFile();
+        return this;
+    }
+
+    public NativeInstallationFixture assertNotInstalled() {
+        installDir.assertDoesNotExist();
+        return this;
+    }
+
+    public NativeInstallationFixture assertIncludesLibraries(String... names) {
+        Set<String> expected = Arrays.asList(names).stream().map(os::getSharedLibraryName).collect(Collectors.toSet());
+        assertThat(getLibraryFiles().stream().map(File::getName).collect(Collectors.toSet()), equalTo(expected));
+        return this;
+    }
+
+    private List<File> getLibraryFiles() {
+        installDir.assertIsDirectory();
+        TestFile libDir = installDir.file("lib");
+        libDir.assertIsDirectory();
+        List<File> libFiles;
+        if (os.isWindows()) {
+            libFiles = Arrays.stream(libDir.listFiles()).filter(it -> it.isFile() && !it.getName().endsWith(".exe")).collect(Collectors.toList());
+        } else {
+            libFiles = Arrays.stream(libDir.listFiles()).filter(it -> it.isFile() && !it.getName().contains(".")).collect(Collectors.toList());
+        }
+        return libFiles;
+    }
+}
