e1088ea951e1b79b614b3ae265f77794190804cf
Daniel Lacasse <daniel@lacasse.io>
2020-07-29 09:51:15 +0200

    Add tests for current Gradle distribution

---
+++ subprojects/gradle-fixtures/src/test/groovy/dev/gradleplugins/test/fixtures/gradle/executer/internal/CurrentGradleDistributionTest.groovy
@@ -0,0 +1,11 @@
+package dev.gradleplugins.test.fixtures.gradle.executer.internal
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+@Subject(CurrentGradleDistribution)
+class CurrentGradleDistributionTest extends Specification {
+    def "refers to current Gradle"() {
+
+    }
+}
