fb467fcc5cfd922ddb9c2ef7e4bcce102707ad5a
Daniel Lacasse <daniel@lacasse.io>
2020-07-30 11:57:40 +0200

    Add test for current Gradle distribution

--- subprojects/gradle-fixtures/src/test/groovy/dev/gradleplugins/test/fixtures/gradle/executer/internal/CurrentGradleDistributionTest.groovy
+++ subprojects/gradle-fixtures/src/test/groovy/dev/gradleplugins/test/fixtures/gradle/executer/internal/CurrentGradleDistributionTest.groovy
@@ -6,6 +6,10 @@ import spock.lang.Subject
 @Subject(CurrentGradleDistribution)
 class CurrentGradleDistributionTest extends Specification {
     def "refers to current Gradle"() {
+        given:
+        def distribution = new CurrentGradleDistribution()
 
+        expect:
+        distribution.gradleHomeDirectory.absolutePath.startsWith("${System.getProperty('user.home')}/.gradle/wrapper/dists/gradle-")
     }
 }
