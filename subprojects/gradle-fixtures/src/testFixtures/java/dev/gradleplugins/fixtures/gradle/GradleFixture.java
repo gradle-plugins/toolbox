486a30d806e77c94099a37be6323238a3ba1cca8
Daniel Lacasse <daniel@lacasse.io>
2020-11-01 17:59:26 -0500

    Introduce generic, interface-only, fixtures for testing

---
+++ subprojects/gradle-fixtures/src/testFixtures/java/dev/gradleplugins/fixtures/gradle/GradleFixture.java
@@ -0,0 +1,7 @@
+package dev.gradleplugins.fixtures.gradle;
+
+import dev.gradleplugins.fixtures.file.FileSystemFixture;
+import dev.gradleplugins.fixtures.gradle.runner.GradleRunnerFixture;
+
+public interface GradleFixture extends GradleScriptFixture, GradleRunnerFixture, FileSystemFixture {
+}
