1413e58687234d5495b2a75552b70c422a742599
Daniel Lacasse <daniel@lacasse.io>
2020-11-02 10:22:15 -0500

    Prepare new version to be released

--- subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/DefaultDependencyVersions.java
+++ subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/DefaultDependencyVersions.java
@@ -3,5 +3,5 @@ package dev.gradleplugins.internal;
 public interface DefaultDependencyVersions {
     String SPOCK_FRAMEWORK_VERSION = "1.2-groovy-2.5";
     String GROOVY_ALL_VERSION = "2.5.2"; // Align with Spock
-    String GRADLE_FIXTURES_VERSION = "0.0.99";
+    String GRADLE_FIXTURES_VERSION = "0.0.100";
 }
