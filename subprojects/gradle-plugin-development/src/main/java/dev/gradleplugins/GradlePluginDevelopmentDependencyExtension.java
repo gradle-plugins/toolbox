98a30c3f34e3afee9592c2597c315faca7ca7b2b
Daniel Lacasse <daniel@lacasse.io>
2021-12-14 21:08:36 -0500

    Update javadoc on dependency extension

--- subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/GradlePluginDevelopmentDependencyExtension.java
+++ subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/GradlePluginDevelopmentDependencyExtension.java
@@ -9,27 +9,31 @@ import org.gradle.api.artifacts.dsl.DependencyHandler;
 public interface GradlePluginDevelopmentDependencyExtension {
     /**
      * Returns the versioned redistributed Gradle API, that is {@literal dev.gradleplugins:gradle-api:$version}, external dependency.
-     * @param version the version of the Gradle API.
-     * @return a dependency instance for the specified version of the Gradle API.
+     *
+     * @param version  the version of the Gradle API, must not be null
+     * @return a dependency instance for the specified version of the Gradle API, never null
      */
     Dependency gradleApi(String version);
 
     /**
      * Returns the versioned redistributed Gradle Test Kit, that is {@literal dev.gradleplugins:gradle-test-kit:$version}, external dependency.
-     * @param version the version of the Gradle Test Kit.
-     * @return a dependency instance for the specified version of the Gradle Test Kit.
+     *
+     * @param version the version of the Gradle Test Kit, must not be null
+     * @return a dependency instance for the specified version of the Gradle Test Kit, never null
      */
     Dependency gradleTestKit(String version);
 
     /**
      * Returns the Gradle Fixtures, that is {@literal dev.gradleplugins:gradle-fixtures:latest.release}, external dependency.
-     * @return a dependency instance for the latest Gradle Fixtures.
+     *
+     * @return a dependency instance for the latest Gradle Fixtures, never null
      */
     Dependency gradleFixtures();
 
     /**
      * Returns the Gradle Runner Kit with all supported executor, that is {@literal dev.gradleplugins:gradle-runner-kit:latest.release}, external dependency.
-     * @return a dependency instance for the latest Gradle Runner Kit with all supported executor.
+     *
+     * @return a dependency instance for the latest Gradle Runner Kit with all supported executor, never null
      */
     Dependency gradleRunnerKit();
 }
