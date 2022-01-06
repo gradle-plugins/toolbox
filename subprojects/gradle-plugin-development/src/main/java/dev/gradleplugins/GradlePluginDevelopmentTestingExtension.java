17377df402ceba4d14e03e67bdac44bc52612e7e
Daniel Lacasse <daniel@lacasse.io>
2022-01-05 20:56:48 -0500

    Add javadoc to testing extension

--- subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/GradlePluginDevelopmentTestingExtension.java
+++ subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/GradlePluginDevelopmentTestingExtension.java
@@ -4,5 +4,11 @@ package dev.gradleplugins;
  * Extension for {@link org.gradle.plugin.devel.GradlePluginDevelopmentExtension}.
  */
 public interface GradlePluginDevelopmentTestingExtension {
+    /**
+     * Registers new test suite of specified name.
+     *
+     * @param name  the test suite name, must not be null
+     * @return a {@link GradlePluginDevelopmentTestSuite} instance, never null
+     */
     GradlePluginDevelopmentTestSuite registerSuite(String name);
 }
