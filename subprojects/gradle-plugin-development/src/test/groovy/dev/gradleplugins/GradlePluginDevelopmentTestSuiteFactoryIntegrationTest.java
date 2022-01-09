e13d5847d47552cf0125b8ed3c917f58214b6560
Daniel Lacasse <daniel@lacasse.io>
2022-01-08 20:41:13 -0500

    Import static test suite factory factory method

--- subprojects/gradle-plugin-development/src/test/groovy/dev/gradleplugins/GradlePluginDevelopmentTestSuiteFactoryIntegrationTest.java
+++ subprojects/gradle-plugin-development/src/test/groovy/dev/gradleplugins/GradlePluginDevelopmentTestSuiteFactoryIntegrationTest.java
@@ -4,12 +4,13 @@ import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.jupiter.api.Test;
 
+import static dev.gradleplugins.GradlePluginDevelopmentTestSuiteFactory.forProject;
 import static dev.gradleplugins.ProjectMatchers.named;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 class GradlePluginDevelopmentTestSuiteFactoryIntegrationTest {
     private final Project project = ProjectBuilder.builder().build();
-    private final GradlePluginDevelopmentTestSuiteFactory subject = GradlePluginDevelopmentTestSuiteFactory.forProject(project);
+    private final GradlePluginDevelopmentTestSuiteFactory subject = forProject(project);
 
     @Test
     void createsTestSuiteUsingName() {
--- subprojects/gradle-plugin-development/src/test/groovy/dev/gradleplugins/GradlePluginDevelopmentTestSuiteFactoryTest.java
+++ subprojects/gradle-plugin-development/src/test/groovy/dev/gradleplugins/GradlePluginDevelopmentTestSuiteFactoryTest.java
@@ -2,11 +2,12 @@ package dev.gradleplugins;
 
 import org.junit.jupiter.api.Test;
 
+import static dev.gradleplugins.GradlePluginDevelopmentTestSuiteFactory.forProject;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GradlePluginDevelopmentTestSuiteFactoryTest {
     @Test
     void throwsExceptionIfProjectIsNullOnFactoryMethod() {
-        assertThrows(NullPointerException.class, () -> GradlePluginDevelopmentTestSuiteFactory.forProject(null));
+        assertThrows(NullPointerException.class, () -> forProject(null));
     }
 }
