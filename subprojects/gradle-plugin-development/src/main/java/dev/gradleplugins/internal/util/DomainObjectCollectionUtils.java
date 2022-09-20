a272984ef1e9f8e0afe296eae663ec55b83d815e
Daniel Lacasse <daniel@lacasse.io>
2022-09-20 12:17:46 -0400

    Add helper method to work with `addAllLater` collection method
    Signed-off-by: Daniel Lacasse <daniel@lacasse.io>

--- subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/util/DomainObjectCollectionUtils.java
+++ subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/util/DomainObjectCollectionUtils.java
@@ -1,7 +1,10 @@
 package dev.gradleplugins.internal.util;
 
 import org.gradle.api.Action;
+import org.gradle.api.provider.Provider;
 import org.gradle.api.specs.Spec;
+
+import java.util.Collections;
 
 public final class DomainObjectCollectionUtils {
     private DomainObjectCollectionUtils() {}
@@ -12,5 +15,16 @@ public final class DomainObjectCollectionUtils {
                 action.execute(it);
             }
         };
+    }
+
+    /**
+     * Helper methods around {@link org.gradle.api.DomainObjectCollection#addAllLater} for optional element.
+     *
+     * @param self  the optional element provider to add, must not be null
+     * @return an single-element or empty iterable provider, never null
+     * @param <ELEMENT>  the element type
+     */
+    public static <ELEMENT> Provider<? extends Iterable<ELEMENT>> singleOrEmpty(Provider<ELEMENT> self) {
+        return self.map(Collections::singletonList).orElse(Collections.emptyList());
     }
 }
