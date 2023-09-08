752a82098d56d04e560dc3f7c9e0e3f33912db8a
MarkRx <MarkRx@users.noreply.github.com>
2023-09-08 12:27:16 -0400

    Add version information up to gradle 8.3
    Signed-off-by: MarkRx <MarkRx@users.noreply.github.com>

--- subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/GradleRuntimeCompatibility.java
+++ subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/GradleRuntimeCompatibility.java
@@ -110,10 +110,16 @@ public final class GradleRuntimeCompatibility {
                 return "3.0.9";
             case "7.5":
                 return "3.0.10";
+            case "7.6":
+            case "8.0":
+                return "3.0.13";
+            case "8.1":
+                return "3.0.15";
             default:
                 LOGGER.warn(String.format("Unknown Groovy version for Gradle '%s', please open an issue on https://github.com/gradle-plugins/toolbox. Assuming value of the latest known version.", gradleVersion.toString()));
-            case "7.6":
-                return "3.0.13";
+            case "8.2":
+            case "8.3":
+                return "3.0.17";
         }
     }
 
@@ -149,6 +155,7 @@ public final class GradleRuntimeCompatibility {
             case 5:
             case 6:
             case 7:
+            case 8:
                 return JavaVersion.VERSION_1_8;
         }
     }
@@ -258,10 +265,17 @@ public final class GradleRuntimeCompatibility {
                 return Optional.of("1.5.31");
             case "7.5":
                 return Optional.of("1.6.21");
-            default:
-                LOGGER.warn(String.format("Unknown Kotlin version for Gradle '%s', please open an issue on https://github.com/gradle-plugins/toolbox. Assuming value of the latest known version.", gradleVersion.toString()));
             case "7.6":
                 return Optional.of("1.7.10");
+            case "8.0":
+            case "8.1":
+                return Optional.of("1.8.10");
+            case "8.2":
+                return Optional.of("1.8.20");
+            default:
+                LOGGER.warn(String.format("Unknown Kotlin version for Gradle '%s', please open an issue on https://github.com/gradle-plugins/toolbox. Assuming value of the latest known version.", gradleVersion.toString()));
+            case "8.3":
+                return Optional.of("1.9.0");
         }
     }
 }
