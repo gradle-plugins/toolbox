01dc953773b540e03fb36653477c34fda1eb405e
Daniel Lacasse <daniel@lacasse.io>
2024-03-08 19:08:40 -0500

    Prepare v1.6.11 release
    Signed-off-by: Daniel Lacasse <daniel@lacasse.io>

--- subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/DefaultDependencyVersions.java
+++ subprojects/gradle-plugin-development/src/main/java/dev/gradleplugins/internal/DefaultDependencyVersions.java
@@ -5,5 +5,5 @@ public interface DefaultDependencyVersions {
     String SPOCK_FRAMEWORK_VERSION = "1.2-groovy-2.5";
     @Deprecated
     String GROOVY_ALL_VERSION = "2.5.2"; // Align with Spock
-    String GRADLE_FIXTURES_VERSION = "0.0.140";
+    String GRADLE_FIXTURES_VERSION = "0.0.141";
 }
